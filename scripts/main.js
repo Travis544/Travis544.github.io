dfbe83a45df4f40a78bab003460738a878fb96a7
Travis Zheng <zhengy6@rose-hulman.edu>
2021-04-18 03:59:41 -0400

    project page

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,8 @@
 
 /** namespace. */
 var rhit = rhit || {};
+rhit.projects=[{name:"Joust", description:"Remake of the classic arcade game joust", imageSrc:"./Image//Joust.JPG", 
+}, {name:"Witcher 3.5", description:"A 2D pixel rpg based on the popular video game series The Witcher", imageSrc:"./Image//Witcher3.5.JPG"}]
 
 /** globals */
 rhit.variableName = "";
@@ -17,13 +19,39 @@ rhit.functionName = function () {
 	/** function body */
 };
 
-rhit.ClassName = class {
-	constructor() {
+//From https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); 
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
 
+
+rhit.ProjectManager = class {
+	constructor(projects) {
+		this.projects=projects
 	}
 
-	methodName() {
+	generateList() {
+		let container=document.querySelector("#projectListContainer")
+		for(let project of this.projects){
+			let card=this._createCard(project)
+			container.appendChild(card)
+		}
+	}
 
+	_createCard(data){
+		return htmlToElement(
+		`<div class="card projectCard">
+			<div class="imageContainer">
+				<img class="card-img-top projectImage" src="${data.imageSrc}" alt="Card image cap">
+			</div>
+			<div class="card-body">
+			<h5 class="card-title">${data.name}</h5>
+			  <p class="card-text">${data.description}</p>
+			</div>
+		  </div>`)
 	}
 }
 
@@ -35,6 +63,7 @@ rhit.determineWhichPage=function(){
 	}else if(document.querySelector("#projectPage")) {
 		document.querySelector("#aboutMeLink").style.color="white"
 		document.querySelector("#projectLink").style.color="orange"
+		rhit.projectManager.generateList();
 	}
 }
 
@@ -42,7 +71,9 @@ rhit.determineWhichPage=function(){
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	rhit.projectManager=new rhit.ProjectManager(rhit.projects);
 	rhit.determineWhichPage();
+
 };
 
 rhit.main();
